3ebfcc024d3cefb7472791bbac8c86c1f61067f9
CoryKDev <corykeelan7@gmail.com>
2021-02-26 18:34:16 -0800

    added functions to clear inputs, and form submition for workout creation

--- main/public/exercise.js
+++ main/public/exercise.js
@@ -74,3 +74,50 @@ function validateInputs() {
             addButton.setAttribute("disabled", true)
         }
 }
+
+async function handleFormSubmit(event) {
+    if (workoutType === "resistance") {
+        workoutData.type = "resistance";
+        workoutData.name = nameInput.value.trim();
+        workoutData.weight = Number(weightInput.value.trim());
+        workoutData.sets = Number(setsInput.value.trim());
+        workoutData.reps = Number(repsInput.value.trim());
+        workoutData.duration = Number(resistanceDurationInput.value.trim());
+      }
+
+      await API.addExercise(workoutData);
+      clearInputs();
+      toast.classList.add("success");
+}
+
+function handleToastAnimationEnd() {
+    toast.removeAttribute("class");
+    if (shouldNavigateAway) {
+        location.href = "/"; 
+    }
+}
+
+function clearInputs(){
+    nameInput.value = "";
+    setsInput.value = "";
+    repsInput.value = "";
+    resistanceDurationInput.value = "";
+    weightInput.value = "";
+
+}
+if (workoutTypeSelect) {
+    workoutTypeSelect.addEventListener("change", handleWorkoutTypeChange);
+}
+if (completeButton){
+    completeButton.addEventListener("click", function (event) {
+        shouldNavigateAway=true;
+        handleFormSubmit(event);
+    });
+}
+if (addButton){
+    addButton.addEventListener("click", handleFormSubmit);
+}
+toast.addEventListener("animationend", handleToastAnimationEnd);
+
+document.querySelectorAll("input")
+.forEach(element => element.addEventListener("input", validateInputs));
